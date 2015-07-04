197e7599cd9f36f31d183b18ef6cbaf347931af0
peterkty <peterkty@gmail.com>
2015-07-04 12:03:55 -0400

    update irb120 urdf

--- install.sh
+++ install.sh
@@ -23,10 +23,8 @@ if [ "$#" == 0 ] || [ "$1" == "APT" ]; then
 
     sudo apt-get --yes install build-essential cmake debhelper freeglut3-dev gtk-doc-tools libboost-filesystem-dev libboost-iostreams-dev libboost-program-options-dev libboost-random-dev libboost-regex-dev libboost-signals-dev libboost-system-dev libboost-thread-dev libcurl4-openssl-dev libfreeimage-dev libglew-dev libgtkmm-2.4-dev libltdl-dev libgsl0-dev libportmidi-dev libprotobuf-dev libprotoc-dev libqt4-dev libqwt-dev libtar-dev libtbb-dev libtinyxml-dev libxml2-dev ncurses-dev pkg-config protobuf-compiler python-matplotlib libvtk5.8 libvtk5-dev libvtk5-qt4-dev libqhull-dev python-pygame doxygen mercurial libglib2.0-dev openjdk-6-jdk python-dev gfortran f2c libf2c2-dev spacenavd libspnav-dev python-numpy python-scipy python-vtk python-pip libgmp3-dev libblas-dev liblapack-dev libv4l-dev subversion libxmu-dev libusb-1.0-0-dev python-pymodbus graphviz curl libwww-perl libterm-readkey-perl g++-4.4
 
-    sudo apt-get --yes install ros-indigo-libpcan # for wsg hand
     sudo apt-get --yes install libopenal-dev # for drake converting wrl file
     sudo apt-get --yes install libgl1-mesa-dev  # for libbot libGL.so
-    sudo apt-get --yes install ros-indigo-industrial-robot-client # for abb driver from ros industrial
     # sudo apt-get --yes install virtualbox
     sudo apt-get --yes install compizconfig-settings-manager
     #sudo pip install --upgrade scipy
@@ -54,6 +52,8 @@ if [ "$#" == 0 ] || [ "$1" == "ROS" ]; then
     sudo apt-get --yes install ros-indigo-pcl-ros
     sudo apt-get --yes install ros-indigo-joy
     sudo apt-get --yes install ros-indigo-perception  # for cv_bridge
+    sudo apt-get --yes install ros-indigo-industrial-robot-client # for abb driver from ros industrial
+    sudo apt-get --yes install ros-indigo-libpcan # for wsg hand
     #sudo apt-get --yes install ros-indigo-moveit-full-pr2
     
 fi
