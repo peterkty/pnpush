2dbbb12c83892266675711b5958e83634dda91c9
nfazeli <nfazeli@mit.edu>
2015-07-10 16:46:57 -0400

    further updates to the delay evaluator

--- catkin_ws/src/pnpush_planning/bag_file_parser.py
+++ catkin_ws/src/pnpush_planning/bag_file_parser.py
@@ -7,14 +7,17 @@ text_file = open("Output_vicon.txt", "w")
 
 for topic, msg, t in bag.read_messages(topics=['/vicon/markers']):
     # print msg
-    text_file.write('%s \n' % msg)
+    text_file.write('%s \n' % msg.header.stamp)
+    text_file.write('%s \n' % msg.markers[-1].translation.x)
+
 text_file.close()
     
 text_file = open("Output_tip.txt", "w")
 
 for topic, msg, t in bag.read_messages(topics=['/tip_pose']):
     # print msg
-    text_file.write('%s \n' % msg)
+    text_file.write('%s \n' % msg.header.stamp)
+    text_file.write('%s \n' % msg.pose.position.x)
 text_file.close()
 bag.close()
 
