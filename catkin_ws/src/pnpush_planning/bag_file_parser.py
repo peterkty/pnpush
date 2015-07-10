f58bc9194c5876dd52c092b0d7ae205f94d65218
nfazeli <nfazeli@mit.edu>
2015-07-10 16:24:41 -0400

    update more for delay between vicon and robot

--- catkin_ws/src/pnpush_planning/bag_file_parser.py
+++ catkin_ws/src/pnpush_planning/bag_file_parser.py
@@ -1,10 +1,20 @@
 
 import rosbag
-bag = rosbag.Bag('tcp_delay.bag')
-text_file = open("Output.txt", "w")
 
-for topic, msg, t in bag.read_messages(topics=['/vicon/markers','/tip_pose']):
+bag = rosbag.Bag('tcp_delay.bag')
+
+text_file = open("Output_vicon.txt", "w")
+
+for topic, msg, t in bag.read_messages(topics=['/vicon/markers']):
     # print msg
     text_file.write('%s \n' % msg)
-bag.close()
 text_file.close()
+    
+text_file = open("Output_tip.txt", "w")
+
+for topic, msg, t in bag.read_messages(topics=['/tip_pose']):
+    # print msg
+    text_file.write('%s \n' % msg)
+text_file.close()
+bag.close()
+
