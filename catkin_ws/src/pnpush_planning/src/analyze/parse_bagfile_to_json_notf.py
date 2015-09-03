e9c24389d8e2db1f9f0907e436051c58b6e2350f
peterkty <peterkty@gmail.com>
2015-09-03 10:44:50 -0400

    added process nimas data

---
+++ catkin_ws/src/pnpush_planning/src/analyze/parse_bagfile_to_json_notf.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+# Peter KT Yu, Aug 2015
+# parse the bagfiles into simple format with timestamp: probe pose, object pose and ft wrench
+
+import rosbag
+import time # for sleep
+import rospy
+import json
+from ik.helper import *
+from sensor_msgs.msg import *
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import TransformStamped
+from tf2_msgs.msg import TFMessage
+import tf
+import tf.transformations as tfm
+
+
+import sys
+import subprocess
+
+
+def ftmsg2list(ftmsg):
+    return [ftmsg.wrench.force.x,ftmsg.wrench.force.y,ftmsg.wrench.force.z,
+            ftmsg.wrench.torque.x,ftmsg.wrench.torque.y,ftmsg.wrench.torque.z]
+
+def ftmsg2list(ftmsg):
+    return [ftmsg.wrench.force.x,ftmsg.wrench.force.y,ftmsg.wrench.force.z,
+            ftmsg.wrench.torque.x,ftmsg.wrench.torque.y,ftmsg.wrench.torque.z]
+            
+def main(argv):
+    global pub
+    global pub_ft
+    global listener
+    global tip_array
+    global object_pose_array
+    global ft_array
+    tip_array = []
+    ft_array = []
+    object_pose_array = []
+    
+    if len(argv) < 2:  # no bagfile name
+        print 'Usage: parse_bagfile_to_json.py [bag_file_path.bag]'
+        print 'Also make sure no roscore is running'
+        return
+    
+        
+    bag_filepath = argv[1]
+    json_filepath = bag_filepath.replace('.bag', '.json')
+    print 'bag_filepath:', bag_filepath
+    print 'json_filepath:', json_filepath
+    
+    
+    
+    #roscore_proc = subprocess.Popen('roscore', shell=True)
+    rospy.init_node('listener', anonymous=True)
+    
+    import rosbag
+    bag = rosbag.Bag(bag_filepath)
+    
+    vicon_to_world = []
+    for topic, msg, t in bag.read_messages(topics=['/tf']):
+        for i in range(len(msg.transforms)):
+            frame_id = msg.transforms[i].header.frame_id
+            child_frame_id = msg.transforms[i].child_frame_id
+            t = msg.transforms[i].transform
+            if child_frame_id == '/viconworld':
+                vicon_to_world = [t.translation.x,t.translation.y,t.translation.z] + [t.rotation.x,t.rotation.y,t.rotation.z,t.rotation.w]
+                break
+        if len(vicon_to_world) > 0: break
+                 
+    
+    T_viconworld_to_world = poselist2mat(vicon_to_world)
+    
+    for topic, msg, t in bag.read_messages(topics=['/vicon/StainlessSteel/StainlessSteel']):
+        pose_viconworld = [msg.transform.translation.x,msg.transform.translation.y,msg.transform.translation.z,
+            msg.transform.rotation.x,msg.transform.rotation.y,msg.transform.rotation.z,msg.transform.rotation.w]
+        
+        T_object_to_viconworld = poselist2mat(pose_viconworld)
+        
+        T_object_to_world = np.dot(T_viconworld_to_world, T_object_to_viconworld)
+        
+        object_to_world = mat2poselist(T_object_to_world)
+        
+        object_pose_array.append([msg.header.stamp.to_sec()] + object_to_world)
+        
+    for topic, msg, t in bag.read_messages(topics=['/robot2_CartesianLog']):
+        #tip_array.append([t.to_sec(), 
+        tip_array.append([msg.timeStamp, 
+            msg.x/1000,msg.y/1000,msg.z/1000,
+            msg.qx,msg.qy,msg.qz,msg.q0])
+        
+    for topic, msg, t in bag.read_messages(topics=['/netft_data']):
+        ft_array.append([msg.header.stamp.to_sec()] + ftmsg2list(msg))
+        
+        
+    bag.close()
+    
+    
+    data = {'tip_poses': tip_array, 'ft_wrench': ft_array, 'object_pose': object_pose_array}
+    
+    # save the data
+    with open(json_filepath, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+        
+    #roscore_proc.terminate()
+
+    
+if __name__=='__main__':
+    main(sys.argv)
+
--- catkin_ws/src/pnpush_planning/src/analyze/plot_json.py
+++ catkin_ws/src/pnpush_planning/src/analyze/plot_json.py
@@ -23,7 +23,8 @@ def plot(data, shape_id, figfname):
 
     fig, ax = plt.subplots()
     probe_radius = 0.004745   # probe1: 0.00626/2 probe2: 0.004745
-    
+    fig.set_size_inches(7,7)
+    #f = plt.figure(figsize=(7, 7))
 
     v = int(figfname.split('_')[-4].split('=')[1])
     sub = int(30 / (v / 20.0))                 # subsample rate
@@ -37,7 +38,7 @@ def plot(data, shape_id, figfname):
     
     # add the object as polygon
     shape_db = ShapeDB()
-    shape_polygon = shape_db.shape_db[shape_id]['shape_poly'] # shape of the objects presented as polygon.
+    shape_polygon = shape_db.shape_db[shape_id]['shape'] # shape of the objects presented as polygon.
     shape_polygon_3d = np.hstack((np.array(shape_polygon), np.zeros((len(shape_polygon), 1)), np.ones((len(shape_polygon), 1))))
     
     object_pose = data['object_pose']
@@ -53,14 +54,14 @@ def plot(data, shape_id, figfname):
         T = matrix_from_xyzquat(object_pose[i][1:4], object_pose[i][4:8])
         shape_polygon_3d_world = np.dot(np.dot(invT0, T), shape_polygon_3d.T)
         
-        obj = mpatches.Polygon(shape_polygon_3d_world.T[:,0:2], closed=True, color='blue', alpha=0.05)
+        obj = mpatches.Polygon(shape_polygon_3d_world.T[:,0:2], closed=True, color='black', alpha=0.8, fill=False, linewidth=1, linestyle='solid')
         ax.add_patch(obj)
         #print 'append', obj
     
     # add the probes as circle
     for i in (range(0, len(tip_pose), sub)):
         tip_pose_0 = np.dot(invT0, tip_pose[i][1:4]+[1])
-        circle = mpatches.Circle(tip_pose_0[0:2], probe_radius, ec="none", color='red', alpha=0.5)
+        circle = mpatches.Circle(tip_pose_0[0:2], probe_radius, color='black', alpha=0.8, fill=False, linewidth=1, linestyle='solid')
         #patches.append(circle)
         ax.add_patch(circle)
 
@@ -68,9 +69,10 @@ def plot(data, shape_id, figfname):
     #collection = PatchCollection(patches, alpha=0.1)
     #ax.add_collection(collection)
     #plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
-    plt.axis([-0.1, 0.1, -0.1, 0.1])
     
-    plt.axis('equal')
+    plt.axis([-0.1, 0.1, -0.1, 0.1])
+    plt.axis('off')
+    #plt.axis('equal')
     #plt.axis('off')
     
     if figfname is not None:
